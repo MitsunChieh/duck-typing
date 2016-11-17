bc38029ec9fbd9539dcebdb259521f21fb0854b8
MitsunChieh <Mitsun.Chieh@gmail.com>
2016-11-17 20:51:53 +0800

    究竟實務上用來做什麼呢？

---
+++ 008.rb
@@ -0,0 +1,15 @@
+class MediaPlayer
+  def play(media)
+    media.play
+  end
+end
+class AudioPlayer
+  def play
+    'play audio'
+  end
+end
+class VideoPlayer
+  def play
+    'play video'
+  end
+end
